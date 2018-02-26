738f1b7c8694ea967b91007f06667390009e535f
Nawar Yossef <nawaryossef2@gmail.com>
2018-02-26 01:09:27 -0500

    fix bug by deleting unexpected token

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-import { Behavior } from '../animalTracker-client/src/components/pages/animal-components/behavior';
+"use strict";
 
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const passport = require('passport');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
 const bodyParser = require("body-parser");
 
-const { router: usersRouter } = require('./users');
-const animalRouter  = require('./animals/routers/animal-router');
-const behaviorRouter  = require('./animals/routers/behavior-router');
-const assessmentRouter  = require('./animals/routers/assessment-router');
+const { router: usersRouter } = require("./users");
+const { router: authRouter, localStrategy, jwtStrategy } = require("./auth");
+const animalRouter = require("./animals/routers/animal-router");
+const behaviorRouter = require("./animals/routers/behavior-router");
+const assessmentRouter = require("./animals/routers/assessment-router");
 
 const { PORT, DATABASE_URL } = require("./config");
-const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
 const app = express();
 mongoose.Promise = global.Promise;
 
 // Logging
-app.use(morgan('common'));
+app.use(morgan("common"));
 
 // CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
     return res.send(204);
   }
   next();
@@ -39,23 +38,23 @@ passport.use(localStrategy);
 passport.use(jwtStrategy);
 
 // ROUTES
-app.use('/api/users/', usersRouter);
-app.use('/api/auth/', authRouter);
-app.use('/animal/', animalRouter);
-app.use('/behavior/', behaviorRouter);
-app.use('/assessment/', assessmentRouter);
+app.use("/api/users/", usersRouter);
+app.use("/api/auth/", authRouter);
+app.use("/animal/", animalRouter);
+app.use("/behavior/", behaviorRouter);
+app.use("/assessment/", assessmentRouter);
 
-const jwtAuth = passport.authenticate('jwt', { session: false });
+const jwtAuth = passport.authenticate("jwt", { session: false });
 
 // A protected endpoint which needs a valid JWT to access it
-app.get('/api/protected', jwtAuth, (req, res) => {
+app.get("/api/protected", jwtAuth, (req, res) => {
   return res.json({
-    data: 'rosebud'
+    data: "rosebud"
   });
 });
 
-app.use('*', (req, res) => {
-  return res.status(404).json({ message: 'Not Found' });
+app.use("*", (req, res) => {
+  return res.status(404).json({ message: "Not Found" });
 });
 
 // Referenced by both runServer and closeServer. closeServer
@@ -73,7 +72,7 @@ function runServer() {
           console.log(`Your app is listening on port ${PORT}`);
           resolve();
         })
-        .on('error', err => {
+        .on("error", err => {
           mongoose.disconnect();
           reject(err);
         });
@@ -84,7 +83,7 @@ function runServer() {
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      console.log('Closing server');
+      console.log("Closing server");
       server.close(err => {
         if (err) {
           return reject(err);
